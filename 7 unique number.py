49d48b80b4d01d8572f71bbd1be5c301c48196f3
Kishanr707 <kishanramesh136@gmail.com>
2026-01-29 06:49:27 +0000

    7 add

--- 7 unique number.py
+++ 7 unique number.py
@@ -1,6 +1,9 @@
+import sys
 n = int(input("Enter the number of elements: "))
 arr = list(map(int, input("Enter the elements: ").split()))
-
+if len(arr) != n:
+    print("Error: Number of elements doesn't match n", file=sys.stderr)
+    exit(1)
 result = 0
 for num in arr:
     result ^= num
