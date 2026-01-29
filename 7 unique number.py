bed353a0c9046a20179bda45c6a0118740157f07
Kishanr707 <kishanramesh136@gmail.com>
2026-01-29 06:27:58 +0000

    lab 7

---
+++ 7 unique number.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number of elements: "))
+arr = list(map(int, input("Enter the elements: ").split()))
+
+result = 0
+for num in arr:
+    result ^= num
+
+print(f"Unique number is {result}")
